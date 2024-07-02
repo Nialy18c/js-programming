e63b86a270cde082c06e40694a1f49ec2eb2309a
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-02 18:39:06 +0000

    converstionOperater

--- 03_converstionOperater.js
+++ 03_converstionOperater.js
@@ -1,3 +1,30 @@
-let score = 33;
-
+let score = null;
+let scor = "33zd";
 console.log(score);
+console.log(typeof scor);
+let valuIsNumber = Number(scor);
+console.log(typeof valuIsNumber);
+console.log(valuIsNumber);
+
+/*
+
+"33" => 33
+"33zd" => Nan
+true => 1 , false => 0
+
+*/
+let isLoggedIn = "nilay";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+/*
+1 => true  , 0 => false
+"" => false
+"nilay" => true
+*/
+let Name = 56;
+console.log(typeof Name);
+let stringNumber = String(Name);
+console.log(typeof stringNumber);
+console.log(stringNumber);
