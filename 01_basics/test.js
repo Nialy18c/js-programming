42d8f0518f9e749b376d36e09cdeb44893eae593
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-02 13:26:35 +0000

    variable done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Nilay");
+const accountId =114245
+let accountEmail = "nilau18@gmail.com";
+var accountPassword = "12456";
+accountCity = "Surat";
+let accountSate;
+
+
+console.log(accountEmail);
+
+accountEmail = "nilay@gmail.com";
+accountPassword = "222222";
+accountCity = "Navsari";
+
+
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountSate]);
