8149b2ed70dd768d87b7e66aadcb7d8ce79c8e17
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-02 23:40:19 +0530

    Add files via upload

---
+++ 02_dataType.js
@@ -0,0 +1,23 @@
+console.log("Nilay");
+console.log(3+3);
+let name = "Nilay";// String DataType
+let age = 19; // Number Data Type
+let isLoggedIn = false; // Boolean Data Type
+
+/*
+number => 2 to power 53
+bigint -> Big Integer
+string => "..." 
+boolean => true/false
+null => Standalone Value
+undefine => 
+symbol => uniqe
+
+***
+object
+
+
+*/
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object
---
+++ test.js
@@ -0,0 +1,17 @@
+const accountId =114245
+let accountEmail = "nilau18@gmail.com";
+var accountPassword = "12456";
+accountCity = "Surat";
+let accountSate;
+
+
+console.log(accountEmail);
+
+accountEmail = "nilay@gmail.com";
+accountPassword = "222222";
+accountCity = "Navsari";
+
+
+
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountSate]);
