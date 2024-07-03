cdbf12a4e58fae6acf2930685e85528907b810fc
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-03 18:38:38 +0000

    String Function

---
+++ 05_string.js
@@ -0,0 +1,20 @@
+const name = "Nilay"
+const count = 50;
+
+console.log(name + count + "value");
+
+console.log('Hello My Name is ${name}and');
+console.log(`Hello My namre is ${name} and My Counter is ${count}`);
+
+const gameName = new String('nilayac')
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('c'));
+
+const anotherString = gameName.substring(0,5);
+console.log(anotherString);
+
+const newString = gameName.slice(-8,4);
