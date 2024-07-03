2bd73dad261906b0996ee145cce9b04d43980e83
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-03 15:15:17 +0000

    operaters

--- 03_converstionOperater.js
+++ 03_converstionOperater.js
@@ -28,3 +28,29 @@ console.log(typeof Name);
 let stringNumber = String(Name);
 console.log(typeof stringNumber);
 console.log(stringNumber);
+
+
+// *****************Operater******************
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/3);
+console.log(2**3);
+console.log(2%3);
+
+let str1 = "Nilay "
+let str2 = "Chauhan"
+ 
+let str3 = str1 + str2 ;
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
