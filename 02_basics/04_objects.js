bdb40f17dd0b22a8a19c479917e8a3ad025437bf
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-08 18:03:16 +0000

    object 2

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,5 +27,27 @@ const obj2 = {3: "a", 4: "b"}
 
 // const obj3 = {obj1, obj2};
 const obj3 = Object.assign({},obj1, obj2)
+const obj4 = {...obj1, ...obj2}
 
-console.log(obj3);
+// console.log(obj4);
+
+const user =[
+    {
+        id:1,
+        email:"hs@gmail.com",
+    },
+    {
+        id:1,
+        email:"hs@gmail.com",
+    },
+    {
+        id:1,
+        email:"hs@gmail.com",
+    },
+]
+
+user[1].email
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
