b5a704d8f9cb513cbf56a927b974d6949c0cdc43
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-11 16:18:13 +0000

    scope and function

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -12,12 +12,50 @@ function myName(){
 //     console.log(num1 + num2);
 // }
 
-function add(num1,num2){
-    let result = num1 + num2;
-    return result;
+// function add(num1,num2){
+//     let result = num1 + num2;
+//     return result;
+// }
+
+// function add(num1,num2){
+//     return num1 + num2;
+// }
+
+function login(username){
+    if(username === ""){
+        console.log("Please Enter a Username");
+    }
+    return `${username} Login is Successfuly..`
 }
 
-function add(num1,num2){
-    return num1 + num2;
+// add(10,"4")
+
+// console.log(login("Nilay"))
+
+function calc(...num){
+    return num
 }
-add(10,"4")
+// console.log(calc(10,20,30))
+
+const user = {
+    username: "Nilay",
+    price:200
+}
+
+function obj(anyobj){
+    console.log(`UserName is ${anyobj.username} and price is ${anyobj.price}`);
+}
+
+obj(user);
+
+const myArry = [200,400,100,600]
+
+function returnarray(getarray){
+    return getarray[0]
+}
+
+// console.log(returnarray(myArry));
+
+function calculate(){
+    return 
+}
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,14 @@
+let a =100
+const b = 20
+var c = 30
+
+if(true){
+    let a = 10
+    console.log("in:",a)
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
