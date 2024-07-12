4114d24e5bcc7342d36b5bf2b36ff8d9cfd319bc
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-12 15:01:15 +0000

    scope for function

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,14 +1,52 @@
-let a =100
-const b = 20
-var c = 30
+// let a =100
+// const b = 20
+// var c = 30
+
+// if(true){
+//     let a = 10
+//     // console.log("in:",a)
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// function one(){
+//     const username = "nilay"
+
+//     function two(){
+//         const web = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(web);
+
+//     two();
+// }
+
+// one();
 
 if(true){
-    let a = 10
-    console.log("in:",a)
+    const uname = "nilay";
+    if(uname === "nilay"){
+        const web =" youtube";
+        console.log(uname + web);
+    }
+    // console.log(web); // Error
 }
+// console.log(uname); //Error
 
-console.log(a);
-console.log(b);
-console.log(c);
+// ++++++++++++++++++++++++++++++interesting ++++++++++++++++++++
+
+// Basic Function
+function addone(num){
+    return num + 1
+}
+console.log(addone(5));
 
 
+// Expretion function
+// console.log(addTwo(5)); // It show the Error from declaring
+const addTwo = function(num){
+    return num + 2 ;
+}
+console.log(addTwo(5));
