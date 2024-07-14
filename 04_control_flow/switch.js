14cdbeff36873740d172fff2f7eea5c1c5ec7f54
Nilay Chauhan <174447641+Nialy18c@users.noreply.github.com>
2024-07-14 18:16:33 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("Januari");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Api");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("Jun");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Agus");
+        break;
+    case 9:
+        console.log("Sep");
+        break;
+    case 10:
+        console.log("Oct");
+        break;
+    case 11:
+        console.log("Nav");
+        break;
+    case 12:
+        console.log("Des");
+        break;
+
+    default:
+        console.log("Not Valid Value");
+        break;
+}
